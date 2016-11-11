290e323d52c57a285d0a8427c07c6ddd75e76bba
Joseph Chambers <joseph@michael-chambers.com>
2016-11-10 22:45:11 -0700

    Had to move the /secret route order is important in express

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ app.use(require('express-session')({
 	resave: false,
 	saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser);
-passport.deserializeUser(User.deserializeUser);
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // =====
 // Routes
@@ -32,6 +33,13 @@ app.get('/', function (req, res) {
 	res.render('home');
 });
 
+app.get('/secret', function (req, res) {
+	console.log('viewing secret route');
+	res.render('secret');
+});
+
+// Auth Routes
+
 //show form
 app.get('/register', function (req, res) {
 	res.render('register');
@@ -39,8 +47,6 @@ app.get('/register', function (req, res) {
 
 //handle user signup
 app.post('/register', function (req, res) {
-	req.body.username
-	req.body.password
 	User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
 		if(err){
 			console.log(err);
@@ -52,22 +58,17 @@ app.post('/register', function (req, res) {
 	});
 });
 
-app.get('/secret', function (req, res) {
-	console.log('viewing secret route');
-	res.render('secret');
-});
-
 // LOGIN ROUTES
-
 app.get('/login', function (req, res) {
 	res.render('login');
 });
 
 // LOGIN LOGIC
+// middleware
 app.post('/login', passport.authenticate('local', {
 	successRedirect: '/secret',
 	failureRedirect: '/login'
-}), function (req, res) {
+}) ,function(req, res){
 });
 
 app.listen(3000, function () {
